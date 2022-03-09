b90a02ac236fd497b4db081f43f18599662af550
MatteoFilippini <matteofilippini8@gmail.com>
2022-03-09 16:24:23 +0100

    fix queryes 1-3

--- db_university.sql
+++ db_university.sql
@@ -1,7 +1,7 @@
 -- 1. Selezionare tutti gli studenti nati nel 1990 (160)
 SELECT * 
 FROM `students` 
-WHERE `date_of_birth` LIKE '1990%'
+WHERE YEAR(`date_of_birth`)= 1990
 
 -- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
 SELECT * 
@@ -11,7 +11,7 @@ WHERE `cfu`>10;
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
 SELECT `name`,`surname`,YEAR(`date_of_birth`) as `anno`
 FROM `students`
-WHERE `anno`>1992;
+WHERE YEAR(`date_of_birth`)>1992
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 SELECT *
