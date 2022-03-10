149fad43170d22d4a6c575678a39fe45298f8106
MatteoFilippini <matteofilippini8@gmail.com>
2022-03-10 15:25:54 +0100

    first q

--- query.sql
+++ query.sql
@@ -1,4 +1,9 @@
 -- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+SELECT `students`.`name`,`students`.`surname`
+FROM `students`
+INNER JOIN `degrees`
+ON `degrees`.`id` = `students`.`degree_id`
+WHERE `degrees`.`name`='Corso di Laurea in Economia'
 
 
 
